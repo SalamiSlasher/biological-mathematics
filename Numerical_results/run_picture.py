a7765b8ea2c7d0079e9c2ce0999043efa362f96e
gudeb <gudebdaN@gmail.com>
2022-02-25 19:49:52 +0300

    dbl

--- Numerical_results/run_picture.py
+++ Numerical_results/run_picture.py
@@ -1,5 +1,9 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
+
+FILE_NAME = 'DBL'
 
 scale_factor = 0.0099
 
@@ -10,7 +14,8 @@ plt.xlim(xmin * scale_factor, xmax * scale_factor)
 #plt.ylim(ymin * scale_factor, ymax * scale_factor)
 
 x = np.linspace(0, 0.15, 4096)
-D = np.genfromtxt('RAND.data')
+D = np.genfromtxt('{}/{}.data'.format(FILE_NAME, FILE_NAME))
+
 plt.plot(x, D[0], label=r'$D_{1, 1}$', scalex=10)
 plt.plot(x, D[1], label=r'$D_{1, 2}$', scalex=10)
 plt.plot(x, D[2], label=r'$D_{1, 3}$', scalex=10)
@@ -18,5 +23,5 @@ plt.plot(x, D[3], label=r'$D_{2, 2}$', scalex=10)
 plt.plot(x, D[4], label=r'$D_{2, 3}$', scalex=10)
 plt.plot(x, D[5], label=r'$D_{3, 3}$', scalex=10)
 plt.legend()
-plt.savefig('Image')
+plt.savefig('{}/Image'.format(FILE_NAME))
 plt.show()
